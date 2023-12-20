1815e00236ff0a5b140202ffb7ead384d1a03106
DayaneLimax <dayanelima.dev@gmail.com>
2023-12-19 22:01:27 -0300

    Projeto da DIO

---
+++ index.js
@@ -0,0 +1,39 @@
+//Desafio Classificador de nível de Herói
+
+let nomeDoHeroi = "AXelleDark";
+let xp = 5316;
+
+
+console.log("=== Seja bem vindo ao Classificador de Nivel de Herói ===")
+
+console.log("Olá " + nomeDoHeroi, ", vou classificar o seu nivel")
+
+if (xp < 1000){
+    xp = "Ferro";
+
+} else if (xp >=1001 && xp <=2000){
+    xp = "Bronze"
+
+} else if (xp >=2001 && xp <=5000){
+    xp = "Prata"
+
+}else if (xp >=5001 && xp <=7000){
+    xp = "Ouro"
+    
+}else if (xp >=7001 && xp <=8000){
+    xp = "Platina"
+
+}else if (xp >= 8001 && xp <=9000){
+    xp = "Ascendente"
+
+}else if (xp >=9001 && xp <=10000){
+    xp = "Imortal"
+
+}else if (xp >=10001){
+    xp = "Radiante"
+
+}
+
+console.log("O Herói de nome " + nomeDoHeroi, " está no nével de " + xp)   
+
+
